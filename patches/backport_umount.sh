ce903c5fd2ac0483d529801d49ef5e9f89c91f95
changhuapeng <changhuapeng@users.noreply.github.com>
2024-02-05 14:13:11 +0800

    Fix patch syntaxes

--- patches/backport_umount.sh
+++ patches/backport_umount.sh
@@ -4,11 +4,11 @@
 # Adapted from https://github.com/tiann/KernelSU/discussions/955#discussioncomment-7854955
 
 # KernelSU/kernel/core_hook.c
-if [ -n "$(grep "// TODO: umount for non GKI kernel" "KernelSU/kernel/core_hook.c")" ]; then
-    ch_line_num=$(grep -n 'int err = path_umount(path, flags);' "KernelSU/kernel/core_hook.c" | awk -F: '{print $1}')
-    ch_line_num_next=$(grep -n '// TODO: umount for non GKI kernel' "KernelSU/kernel/core_hook.c" | awk -F: '{print $1}')
-    lines_to_del="$(($ch_line_num-1))d;$(($ch_line_num_next-1)),$(($ch_line_num_next+1))d"
-    sed -i "$lines_to_del" "KernelSU/kernel/core_hook.c"
+if grep -q "// TODO: umount for non GKI kernel" KernelSU/kernel/core_hook.c; then
+    ch_line_num=$(grep -n "int err = path_umount(path, flags);" KernelSU/kernel/core_hook.c | awk -F: '{print $1}')
+    ch_line_num_next=$(grep -n "// TODO: umount for non GKI kernel" KernelSU/kernel/core_hook.c | awk -F: '{print $1}')
+    lines_to_del="$((ch_line_num - 1))d;$((ch_line_num_next - 1)),$((ch_line_num_next + 1))d"
+    sed -i "$lines_to_del" KernelSU/kernel/core_hook.c
 fi
 
 # fs/namespace.c
@@ -48,13 +48,12 @@ int path_umount(struct path *path, int flags)
 __EOF__
 )
 
+if ! grep -q "static int can_umount" fs/namespace.c; then
+    ns_line_num=$(grep -n "\*.*Now umount can handle mount points as well as block devices\." fs/namespace.c | awk -F: '{print $1}')
 
-if [ -z "$(grep "static int can_umount" "fs/namespace.c")" ]; then
-    ns_line_num=$(grep -n '\*.*Now umount can handle mount points as well as block devices\.' "fs/namespace.c" | awk -F: '{print $1}')
-
-    if [[ $(sed "$(($ns_line_num-1))"'!d' "fs/namespace.c") = "/*" ]]; then
+    if [[ $(sed "$((ns_line_num - 1))"'!d' fs/namespace.c) = "/*" ]]; then
         preprocessed_PATCH=$(printf '%s\n' "$PATCH" | sed 's/\\/&&/g;s/^[[:blank:]]/\\&/;s/$/\\/')
-        sed -i -e "$(($ns_line_num-1))"'i \'"${preprocessed_PATCH%?}" "fs/namespace.c"
+        sed -i -e "$((ns_line_num - 1))"'i \'"${preprocessed_PATCH%?}" fs/namespace.c
     fi
 fi
 
